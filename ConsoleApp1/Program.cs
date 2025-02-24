0805a2f2356d5adaa02d7ae2efa1c53a0e89bebb
Snape-bur <supramk976@gmail.com>
2025-02-24 15:12:13 +0700

    My first C# project upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(); // Wait for user input before closing
+
+
+        }
+    }
+}
